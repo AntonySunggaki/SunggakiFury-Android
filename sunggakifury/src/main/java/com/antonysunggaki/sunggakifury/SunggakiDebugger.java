1948da2c5768b1811ea03cbfb90561809d4f9ee7
antonysunggaki <32890225+AntonySunggaki@users.noreply.github.com>
2020-01-04 10:46:36 +0700

    Update SunggakiDebugger.java

--- sunggakifury/src/main/java/com/antonysunggaki/sunggakifury/SunggakiDebugger.java
+++ sunggakifury/src/main/java/com/antonysunggaki/sunggakifury/SunggakiDebugger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 ZXing authors
+ * Copyright 2019 AntonySunggaki authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
