8d78761349c7d3ae07f9f63b2bb3861a5bbf99e9
AntonySunggaki <antonystarsas@gmail.com>
2020-01-04 09:28:30 +0700

    Create project in Studio, Create LogDebug for easy debugging.

---
+++ sunggakifury/src/main/java/com/antonysunggaki/sunggakifury/SunDebugger.java
@@ -0,0 +1,12 @@
+package com.antonysunggaki.sunggakifury;
+
+import android.util.Log;
+
+public class SunDebugger {
+
+    private static final String TAG = "XSunggakiFuryX";
+
+    public static void d(String message){
+        Log.d(TAG, message);
+    }
+}
